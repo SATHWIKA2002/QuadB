c13db71b5846a441ef8206d9da4a6731ab043a71
SATHWIKA2002 <98942206+SATHWIKA2002@users.noreply.github.com>
2024-05-31 20:53:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import TaskList from "./components/TaskList/TaskList";
+import TaskForm from "./components/TaskForm/TaskForm";
+import EditTaskModal from "./components/EditTaskModal/EditTaskModal";
+import "./App.css";
+
+const App = () => {
+  const [editingTask, setEditingTask] = useState(null);
+
+  return (
+    <Provider store={store}>
+      <div className="App text-center p-4">
+        <h1 className="main-title mb-4">Task Manager</h1>
+        <div className="developer-container">
+        
+            <p className="para">TODOLIST</p>
+       
+        </div>
+        <TaskForm />
+        <TaskList onEdit={setEditingTask} />
+        {editingTask && (
+          <EditTaskModal
+            task={editingTask}
+            onClose={() => setEditingTask(null)}
+          />
+        )}
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
